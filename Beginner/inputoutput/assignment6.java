fb0789a04b22ce7f6b4a21068e452a08f7b40b92
manipriyan77 <manipriyangopalan@gmail.com>
2022-07-14 08:44:39 +0530

    finished assignments of input output

--- Beginner/inputoutput/assignment6.java
+++ Beginner/inputoutput/assignment6.java
@@ -1,5 +1,16 @@
+/*You're going to write some code to help you cook a gorgeous lasagna from your favorite cookbook.
+Now, you want to also add few layers to the lasagna. Assume each layer takes 2 minutes to prepare.
+
+Given the number of layers you want to add to the lasagna, find how many minutes you would spend making them. */
+
 package Beginner.inputoutput;
+import java.util.*;;
 
 public class assignment6 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int min = sc.nextInt();
+        System.out.print(min*2);
+    }
     
 }
--- Beginner/inputoutput/assignment7.java
+++ Beginner/inputoutput/assignment7.java
@@ -1,5 +1,15 @@
-package Beginner.inputoutput;
+/*You're going to write some code to help you cook a gorgeous lasagna from your favorite cookbook.
+Now, you want to find the total number of minutes you've been cooking, or the sum of your preparation time and the time the lasagna has already spent baking in the oven.
 
+Given the number of layers added to the lasagna and the number of minutes the lasagna has been baking in the oven, find the total elapsed cooking time (prep + bake) in minutes. */
+package Beginner.inputoutput;
+import java.util.*;
 public class assignment7 {
+    public static void main(String[]args){
+        Scanner value = new Scanner(System.in);
+        int min = value.nextInt();
+        int bake = value.nextInt();
+        System.out.print((min*2)+bake);
+    }
     
 }
